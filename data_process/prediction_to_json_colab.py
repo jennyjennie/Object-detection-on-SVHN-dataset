2846f67bbc8c015e2de3ad4097fd7dc130838592
jennyjennie <jennyho0221@gmail.com>
2021-11-23 23:32:38 +0800

    prediction to json colab

---
+++ data_process/prediction_to_json_colab.py
@@ -0,0 +1,27 @@
+"""
+Change category_id from 10 to 1 and generate the answer.json
+"""
+
+# Imports
+import json
+import os
+
+ans_json_file = './prediction/prediction.bbox.json'
+
+with open(ans_json_file, 'r', encoding='utf-8') as json_file:
+    data = json.load(json_file)
+
+for d in data:
+    if d['category_id'] == 10:
+        d['category_id'] == 0
+
+json_file_path = '../answer.json'
+
+os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+json_fp = open(json_file_path, "w")
+
+json_str = json.dumps(data, indent=4)
+json_fp.write(json_str)
+json_fp.close()
+
+print("Success")
